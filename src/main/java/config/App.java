4a9d9acf5874cb18381f0e0eceb8bce640138dbd
narushimas <narushima.shigeki7@gmail.com>
2021-05-15 19:16:09 +0900

    finish

--- src/main/java/config/App.java
+++ src/main/java/config/App.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
